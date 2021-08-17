1d9a7c386ba399b7192deb7f94f5b4c34031e8b0
nguyenbong15 <nguyenbong20001@gmail.com>
2021-08-17 20:06:11 +0700

    edit

--- src/main/java/common/AbtractPage.java
+++ src/main/java/common/AbtractPage.java
@@ -12,7 +12,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 public class AbtractPage {
-
+///
 	public void openUrl(WebDriver driver, String url) {
 		driver.get(url);
 	}
